8e8028c8688e9179a3a26ba7fcd1aac1aee143f7
Ludan Stoeckle <ludan.stoeckle@gmail.com>
2020-05-05 09:13:05 +0200

    1.15.0

--- gulpfile.js/package.js
+++ gulpfile.js/package.js
@@ -1,29 +1,34 @@
-const gulpRosaeNLG = require('gulp-rosaenlg');
+const packager = require('rosaenlg-packager');
+const rosaenlg = require('rosaenlg');
 const fs = require('fs');
 
 function doPackage(cb) {
 
-  const packageObj = gulpRosaeNLG.packageTemplateJson({
+  const packageObj = {
     templateId: 'phones',
-    entryTemplate: 'templates/phoneForJson.pug',
-    compileInfo: {
-      language: 'fr_FR'
-    },
-    autotest: {
-      activate: true,
-      input: {
-        language: 'fr_FR',
-        phone: {
-          "name": "OnePlus 5T",
-          "colors": ["Black", "Red", "White"],
-          "displaySize": 6,
-          "screenRatio": 80.43,
-          "battery": 3300
-        }
+    src: {
+      entryTemplate: 'templates/phoneForJson.pug',
+      compileInfo: {
+        language: 'fr_FR'
       },
-      expected: ['phone', 'battery']
+      autotest: {
+        activate: true,
+        input: {
+          language: 'fr_FR',
+          phone: {
+            "name": "OnePlus 5T",
+            "colors": ["Black", "Red", "White"],
+            "displaySize": 6,
+            "screenRatio": 80.43,
+            "battery": 3300
+          }
+        },
+        expected: ['phone', 'battery']
+      }
     }
-  });
+  }
+  
+  packager.completePackagedTemplateJson(packageObj, rosaenlg);
 
   fs.writeFile('dist/phone_package.json', JSON.stringify(packageObj), 'utf8', () => {
     cb();
